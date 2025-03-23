c755b4c998669e179c07bdc2ac6866d674edd1be
Tamal <ckbtamaldipnew@gmail.com>
2025-03-23 20:36:42 +0600

    Extension -> rankDevs -> add apiKey setter and trackTyping function

--- vscode-rankdevs/src/rankDevs.ts
+++ vscode-rankdevs/src/rankDevs.ts
@@ -1,2 +1,35 @@
 import * as vscode from 'vscode';
-import {sendTypingDataToBackend} from './apiService';
+import { sendTypingDataToBackend } from './apiService';
+
+export class RankDevs{
+    private context: vscode.ExtensionContext;
+    private typingStartTime: number | null = null;
+    private isTyping: boolean = false;
+
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+    }
+
+    public setApiKey(apiKey: string): void{
+        this.context.globalState.update('rankDevsApiKey', apiKey);
+        vscode.window.showInformationMessage("API Key set successfully");
+    }
+
+    public trackTyping(event: vscode.TextDocumentChangeEvent): void{
+        if (!this.isTyping) {
+            this.typingStartTime = Date.now();
+            this.isTyping = true;
+            console.log("Typing started!");
+        }
+
+        setTimeout(() => {
+            const now = Date.now();
+            if (now - this.typingStartTime! >= 10000) {
+                this.stopTracking();
+            }
+        }, 10000);
+    }
+
+    private stopTracking():void{}
+
+}
