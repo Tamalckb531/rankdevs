c02d9289ac7f6d4628ab419de15872f0a707651a
Tamal <ckbtamaldipnew@gmail.com>
2025-03-23 20:11:49 +0600

    Extension -> set-up -> set file structure

--- vscode-rankdevs/src/extension.ts
+++ vscode-rankdevs/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('vscode-rankdevs.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-rankdevs!');
+		vscode.window.showInformationMessage('This is Tamal');
 	});
 
 	context.subscriptions.push(disposable);
