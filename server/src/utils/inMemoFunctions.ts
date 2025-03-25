b45fac66a6dfba114e14b5a1cd4169851513185b
Tamal <ckbtamaldipnew@gmail.com>
2025-03-25 10:24:12 +0600

    Leaderboard feature -> backend -> add old time cleanup logic

--- server/src/utils/inMemoFunctions.ts
+++ server/src/utils/inMemoFunctions.ts
@@ -11,3 +11,23 @@ export const updateStats = (stats: Stats, typingTime: number, language: string) 
     //? adding a log to keep track of sliding time
     stats.logs.push({ typingTime, language, timestamp: now });
 }
+
+export const cleanOldStats = (stats: Stats, windowInMillis: number) => {
+    const cutoffTime = Date.now() - windowInMillis;
+
+    //? Keep only logs from within the sliding window (last 24 hours, 7 days, or 30 days)
+    stats.logs = stats.logs.filter(log => log.timestamp > cutoffTime);
+
+    //? language and totalTime recalculation
+    stats.total = 0;
+    for (const key in stats) {
+        if (key !== 'total' && key !== 'logs') {
+            stats[key] = 0;
+        }
+    }
+
+    stats.logs.forEach(log => {
+        stats.total += log.typingTime;
+        stats[log.language] = (stats[log.language] || 0) + log.typingTime;
+    });
+}
