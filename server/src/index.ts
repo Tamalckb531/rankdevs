636e63643a9e20c5b6f2fb59415837e19d3c9bb7
Tamal <ckbtamaldipnew@gmail.com>
2025-03-25 10:30:17 +0600

    Leaderboard feature -> backend -> add interval wise cleanup function

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import { cors } from 'hono/cors';
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import leaderboardRoute from './routes/leaderboard.route.js'
+import { inMemoryStats } from './utils/inMemoryStats.js';
+import { cleanOldStats } from './utils/inMemoFunctions.js';
 
 dotenv.config();
 
@@ -37,6 +39,22 @@ app.onError((err:any, c) => {
     statuscode
   );
 });
+
+//? interval wise cleanup 
+const ONE_DAY = 24 * 60 * 60 * 1000;   
+const ONE_WEEK = 7 * ONE_DAY;          
+const ONE_MONTH = 30 * ONE_DAY;     
+
+setInterval(() => {
+  for (const userId in inMemoryStats) {
+      const { dailyStats, weeklyStats, monthlyStats } = inMemoryStats[userId];
+
+      cleanOldStats(dailyStats, ONE_DAY);      // Clean stats older than 24 hours
+      cleanOldStats(weeklyStats, ONE_WEEK);    // Clean stats older than 7 days
+      cleanOldStats(monthlyStats, ONE_MONTH);  // Clean stats older than 30 days
+  }
+  console.log("Old logs cleaned!");
+}, 60 * 1000);
 
 //? server
 serve({
