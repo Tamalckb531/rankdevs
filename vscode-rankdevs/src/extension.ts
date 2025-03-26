a304f01172254aa3957527ea336f115cd6de6d80
Tamal <ckbtamaldipnew@gmail.com>
2025-03-26 11:49:23 +0600

    leaderboard feature -> extension -> understand extension.ts

--- vscode-rankdevs/src/extension.ts
+++ vscode-rankdevs/src/extension.ts
@@ -1,17 +1,18 @@
 import * as vscode from 'vscode';
 import { RankDevs } from './rankDevs';
 
+//? run as soon as user open vs code
 export function activate(context: vscode.ExtensionContext) {
 	console.log('RankDevs extension is now active!');
 
 	const rankDevs = new RankDevs(context);
 
-	// Track user typing by registering the change event in the active editor
-
+	//? Run whenever user change something in their codebase
 	context.subscriptions.push(
 		vscode.workspace.onDidChangeTextDocument((event) => rankDevs.trackTyping(event))
 	)
 
+	//? run when user run the setApiKey command
 	const disposable = vscode.commands.registerCommand('vscode-rankdevs.setApiKey', () => {
 		
 		vscode.window.showInputBox({
@@ -21,10 +22,11 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
+	//? to clean up the register command
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+//? run as soon as user close vs code
 export function deactivate() {
 	console.log('RankDevs extension is now deactivated!');
 }
