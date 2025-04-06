5e4671528cea74483366e0fa08a29d4208e89264
Tamal <ckbtamaldipnew@gmail.com>
2025-04-06 07:19:58 +0600

    testing shadcn and tailwind done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className=" bg-black text-white h-[100vh]">
+      Hello <Button className="mt-5 ml-5 bg-white text-black">Click me</Button>
+    </div>
+  );
 }
