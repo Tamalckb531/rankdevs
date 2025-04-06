87e7b2f354b487b041e922c3fef13cd6b609cb8e
Tamal <ckbtamaldipnew@gmail.com>
2025-04-06 20:34:42 +0600

    Leaderboard -> Design -> Add leaderboard page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,22 +1,26 @@
+"use client";
 import React from "react";
 import Logo from "./header/Logo";
 import { ModeToggle } from "./header/ToggleBtn";
 import { Trophy } from "lucide-react";
 import { AvatorDropDown } from "./header/AvatorDropDown";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className=" flex items-center justify-between p-2 mt-1 md:mx-3">
       <div className=" logo flex justify-start items-center">
         <Logo />
       </div>
       <nav className=" nav flex items-center justify-between gap-3">
-        <p
+        <div
           className=" hover:dark:bg-[#1B1B1D] hover:bg-gray-100 p-2 rounded-md transition-colors duration-300"
           title="Leaderboard"
+          onClick={() => router.push("/leaderboard")}
         >
           <Trophy size={20} />
-        </p>
+        </div>
         <ModeToggle />
         <AvatorDropDown />
       </nav>
--- client/src/components/header/Logo.tsx
+++ client/src/components/header/Logo.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Logo = () => {
+  const router = useRouter();
   return (
-    <h1 className="text-2xl font-bold bg-gradient-to-r from-rose-500 via-pink-500 to-fuchsia-500 bg-clip-text text-transparent cursor-pointer">
+    <h1
+      className="text-2xl font-bold bg-gradient-to-r from-rose-500 via-pink-500 to-fuchsia-500 bg-clip-text text-transparent cursor-pointer"
+      onClick={() => router.push("/")}
+    >
       RankDevs
     </h1>
   );
