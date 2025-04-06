e75312a1a42090ad32050609f00e2a1747ea06c7
Tamal <ckbtamaldipnew@gmail.com>
2025-04-06 07:30:26 +0600

    add next theme

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className=" bg-black text-white h-[100vh]">
-      Hello <Button className="mt-5 ml-5 bg-white text-black">Click me</Button>
+    <div className="">
+      Hello <Button className="mt-5 ">Click me</Button>
     </div>
   );
 }
