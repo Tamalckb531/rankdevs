9a1658aab2392f2a6d44ad789d00d1697cf60465
Tamal <ckbtamaldipnew@gmail.com>
2025-04-06 07:33:45 +0600

    add theme toggle button

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/ToggleBtn";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       Hello <Button className="mt-5 ">Click me</Button>
+      <ModeToggle />
     </div>
   );
 }
