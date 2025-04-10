0a1755dddd4644e3ead7eb6ebfcf280f8a6d7202
Tamal <ckbtamaldipnew@gmail.com>
2025-04-10 18:22:27 +0600

    Leaderboard feature -> design done

--- client/src/app/leaderboard/page.tsx
+++ client/src/app/leaderboard/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <div className=" flex flex-col md:mx-3 p-2 ">
       <LeaderboardTitleBar />
-      <div className=" flex flex-col gap-2 items-center justify-center mt-5 h-full">
+      <div className=" flex flex-col gap-2 items-center justify-center h-full">
         <LeaderboardCardWrapper />
         <LeaderboardContent />
       </div>
