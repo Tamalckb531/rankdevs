734199662d040656b6ca571a5b45f05f158bf6e0
Tamal <ckbtamaldipnew@gmail.com>
2025-04-11 18:42:28 +0600

    dashboard feature -> design -> add dashboard grid container

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import DashboardGridContainer from "@/components/DashboardGridContainer";
 import React from "react";
 
 const page = () => {
-  return <div>Welcome to dashboard</div>;
+  return (
+    <div className=" flex items-center justify-center">
+      <DashboardGridContainer />
+    </div>
+  );
 };
 
 export default page;
---
+++ client/src/components/DashboardGridContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardGridContainer = () => {
+  return (
+    <div className=" border-5 w-full lg:w-[80rem] h-full mt-5">
+      DashboardGridContainer
+    </div>
+  );
+};
+
+export default DashboardGridContainer;
