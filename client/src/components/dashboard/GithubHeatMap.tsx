a31552e6faf85248e62e8afd3dd4b8fce6917a0b
Tamal <ckbtamaldipnew@gmail.com>
2025-04-13 00:46:41 +0600

    dashboard feature -> design -> add Github Heatmap

--- client/src/components/dashboard/GithubHeatMap.tsx
+++ client/src/components/dashboard/GithubHeatMap.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 const GithubHeatMap = () => {
-  return <div className=" hidden md:block md:col-span-2">GithubHeatMap</div>;
+  return (
+    <Card className="hidden md:flex flex-col md:col-span-2 bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2">
+      <CardHeader className="w-full flex items-center justify-between -mt-1">
+        <CardTitle className="text-xl font-bold">Github Heatmap</CardTitle>
+      </CardHeader>
+      <CardContent className=" flex flex-col items-center justify-center h-full">
+        <GitHubCalendar username="Tamalckb531" />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default GithubHeatMap;
+
+//?hidden md:block md:col-span-2
