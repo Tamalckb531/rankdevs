052428c0496e50a04139e38d0d43e79a4ee6ea5c
Tamal <ckbtamaldipnew@gmail.com>
2025-04-12 23:56:48 +0600

    dashboard feature -> design -> add Github box set-up

--- client/src/components/dashboard/GithubBox.tsx
+++ client/src/components/dashboard/GithubBox.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Card, CardHeader, CardTitle } from "../ui/card";
 
 const GithubBox = () => {
-  return <div>GithubBox</div>;
+  return (
+    <Card className=" flex flex-col bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2">
+      <CardHeader className="w-full flex items-center justify-between -mt-1">
+        <CardTitle className="text-xl font-bold">Github Stats</CardTitle>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default GithubBox;
