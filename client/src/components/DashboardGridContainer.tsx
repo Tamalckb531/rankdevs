3eb10cb1bdd2b939b968a2b4a587a46ad7a7acb1
Tamal <ckbtamaldipnew@gmail.com>
2025-04-13 10:29:24 +0600

    dashboard feature -> design -> change in grid

--- client/src/components/DashboardGridContainer.tsx
+++ client/src/components/DashboardGridContainer.tsx
@@ -10,7 +10,7 @@ import YearlyStats from "./dashboard/YearlyStats";
 
 const DashboardGridContainer = () => {
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 border-5 w-full lg:w-[90rem] h-full mt-5 p-3">
+    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full lg:w-[90rem] h-full mt-5 p-3">
       <UserProfile />
       <TotalStats />
       <CPStats />
