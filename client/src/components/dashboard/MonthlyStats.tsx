9904e6076e252abc674f671c65f5d0ab8bf4c97d
Tamal <ckbtamaldipnew@gmail.com>
2025-04-13 21:29:12 +0600

    dashboard feature -> design -> add monthly stats structure

--- client/src/components/dashboard/MonthlyStats.tsx
+++ client/src/components/dashboard/MonthlyStats.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import StatSelect from "./StatsComp/StatSelect";
+import { WeeklyPieChart } from "./WeeklyComp/weeklyPieChart";
 
 const MonthlyStats = () => {
   return (
-    <div className=" border md:col-span-2 lg:col-span-3">MonthlyStats</div>
+    <Card className="flex flex-col md:col-span-2 lg:col-span-3 bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2 ">
+      <CardHeader className="w-full flex items-center justify-between -mt-1">
+        <CardTitle className="text-xl font-bold">Monthly Stats</CardTitle>
+        <StatSelect />
+      </CardHeader>
+      <CardContent>
+        <WeeklyPieChart />
+      </CardContent>
+    </Card>
   );
 };
 
--- client/src/components/dashboard/WeeklyStats.tsx
+++ client/src/components/dashboard/WeeklyStats.tsx
@@ -5,7 +5,7 @@ import { WeeklyPieChart } from "./WeeklyComp/weeklyPieChart";
 
 const WeeklyStats = () => {
   return (
-    <Card className=" border flex flex-col bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2 ">
+    <Card className="flex flex-col bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2 ">
       <CardHeader className="w-full flex items-center justify-between -mt-1">
         <CardTitle className="text-xl font-bold">Weekly Stats</CardTitle>
         <StatSelect />
