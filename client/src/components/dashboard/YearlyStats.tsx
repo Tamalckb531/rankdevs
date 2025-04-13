462ce2176e01540b7d11ac586fd0eaa82db398e5
Tamal <ckbtamaldipnew@gmail.com>
2025-04-13 21:42:27 +0600

    dashboard feature -> design -> add yearly stats structure

--- client/src/components/dashboard/YearlyStats.tsx
+++ client/src/components/dashboard/YearlyStats.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import StatSelect from "./StatsComp/StatSelect";
+import { WeeklyPieChart } from "./WeeklyComp/weeklyPieChart";
 
 const YearlyStats = () => {
-  return <div className=" lg:col-span-2 border">YearlyStats</div>;
+  return (
+    <Card className="flex flex-col lg:col-span-2 bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2 ">
+      <CardHeader className="w-full flex items-center justify-between -mt-1">
+        <CardTitle className="text-xl font-bold">Yearly Stats</CardTitle>
+        <StatSelect />
+      </CardHeader>
+      <CardContent>
+        <WeeklyPieChart />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default YearlyStats;
