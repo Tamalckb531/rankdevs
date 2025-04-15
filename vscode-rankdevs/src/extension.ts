fd9b454ce5c6232508ddf0bd396b8b44344c322b
Tamal <ckbtamaldipnew@gmail.com>
2025-04-15 21:43:14 +0600

    extension -> started debuggin

--- vscode-rankdevs/src/extension.ts
+++ vscode-rankdevs/src/extension.ts
@@ -1,7 +1,6 @@
-import * as vscode from 'vscode';
-import { RankDevs } from './rankDevs';
-import { StatsManager } from './StatsManager';
-
+import * as vscode from "vscode";
+import { RankDevs } from "./rankDevs";
+import { StatsManager } from "./StatsManager";
 
 let globalContext: vscode.ExtensionContext;
 let intervalIdForCleanup: NodeJS.Timeout;
@@ -9,46 +8,56 @@ let intervalIdForApiCall: NodeJS.Timeout;
 
 //? run as soon as user open vs code
 export function activate(context: vscode.ExtensionContext) {
-	console.log('RankDevs extension is now active!');
-	globalContext = context;
+  console.log("Hi Tamal! RankDevs extension is now active!");
+  globalContext = context;
 
-	const rankDevs = new RankDevs(context);
+  const rankDevs = new RankDevs(context);
 
-	//? Run whenever user change something in their codebase
-	context.subscriptions.push(
-		vscode.workspace.onDidChangeTextDocument((event) => rankDevs.trackTyping(event))
-	)
+  //? Run whenever user change something in their codebase
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((event) => {
+      console.log("Yo! User typed shit!");
 
-	//? run when user run the setApiKey command
-	const disposable = vscode.commands.registerCommand('vscode-rankdevs.setApiKey', () => {
-		
-		vscode.window.showInputBox({
-			prompt: 'Enter your Api Key : '
-		}).then((apiKey) => {
-			if (apiKey) rankDevs.setApiKey(apiKey);
-		});
-	});
+      rankDevs.trackTyping(event);
+    })
+  );
 
-	//? to clean up the register command
-	context.subscriptions.push(disposable);
+  //? run when user run the setApiKey command
+  const disposable = vscode.commands.registerCommand(
+    "vscode-rankdevs.setApiKey",
+    () => {
+      console.log("Yo! User setting api key");
 
-	// **Interval-Based Cleanup for Daily/Weekly/Monthly Logs**
-    intervalIdForCleanup = setInterval(() => {
-        const manager = StatsManager.getInstance(globalContext);
-		manager.intervalWiseCleanUp();
-	}, 10 * 60 * 1000); // Run every 10 minutes
-	
-	// **Interval-Based api call**
-	intervalIdForApiCall = setInterval(() => {
-		rankDevs.callApiService();
-	}, 2 * 60 * 1000); // Run every 2 minutes
+      vscode.window
+        .showInputBox({
+          prompt: "Enter your Api Key : ",
+        })
+        .then((apiKey) => {
+          if (apiKey) rankDevs.setApiKey(apiKey);
+        });
+    }
+  );
 
+  //? to clean up the register command
+  context.subscriptions.push(disposable);
+
+  // **Interval-Based Cleanup for Daily/Weekly/Monthly Logs**
+  // intervalIdForCleanup = setInterval(() => {
+  //     const manager = StatsManager.getInstance(globalContext);
+  // 	manager.intervalWiseCleanUp();
+  // }, 10 * 60 * 1000); // Run every 10 minutes
+
+  // **Interval-Based api call**
+  // intervalIdForApiCall = setInterval(() => {
+  // 	rankDevs.callApiService();
+  // }, 2 * 60 * 1000); // Run every 2 minutes
 }
 
 //? run as soon as user close vs code
 export function deactivate() {
-	clearInterval(intervalIdForCleanup);
-	clearInterval(intervalIdForApiCall);
-	const manager = StatsManager.getInstance(globalContext);
-	manager.cleanup(globalContext)
+  // clearInterval(intervalIdForCleanup);
+  // clearInterval(intervalIdForApiCall);
+  // const manager = StatsManager.getInstance(globalContext);
+  // manager.cleanup(globalContext)
+  console.log("RankDevs got de-active ");
 }
--- vscode-rankdevs/src/rankDevs.ts
+++ vscode-rankdevs/src/rankDevs.ts
@@ -1,74 +1,75 @@
-import * as vscode from 'vscode';
-import { sendTypingDataToBackend } from './apiService';
-import { StatsManager } from './StatsManager';
+import * as vscode from "vscode";
+import { sendTypingDataToBackend } from "./apiService";
+import { StatsManager } from "./StatsManager";
 
 //? test
 
+export class RankDevs {
+  private context: vscode.ExtensionContext;
+  private typingStartTime: number | null = null;
+  private typingStartLan: string = "";
+  private typingTimer: NodeJS.Timeout | null = null; // Timer reference
+  private isTyping: boolean = false;
 
-export class RankDevs{
-    private context: vscode.ExtensionContext;
-    private typingStartTime: number | null = null;
-    private typingStartLan: string = "";
-    private typingTimer: NodeJS.Timeout | null = null; // Timer reference
-    private isTyping: boolean = false;
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+  }
 
-    constructor(context: vscode.ExtensionContext) {
-        this.context = context;
+  public setApiKey(apiKey: string): void {
+    this.context.globalState.update("rankDevsApiKey", apiKey);
+    vscode.window.showInformationMessage("API Key set successfully");
+  }
+
+  //? Store the first typed time of a frequent typed code inside typingStartTime within less than 10 seconds break
+  public trackTyping(event: vscode.TextDocumentChangeEvent): void {
+    const currentLanguage =
+      vscode.window.activeTextEditor?.document.languageId || "unknown";
+
+    if (!this.isTyping) {
+      this.typingStartTime = Date.now();
+      this.isTyping = true;
+      this.typingStartLan = currentLanguage;
+      console.log("Typing started!");
     }
 
-    public setApiKey(apiKey: string): void{
-        this.context.globalState.update('rankDevsApiKey', apiKey);
-        vscode.window.showInformationMessage("API Key set successfully");
+    if (this.typingTimer) clearTimeout(this.typingTimer);
+    if (currentLanguage !== this.typingStartLan) {
+      console.log("User change the language");
+      // this.stopTracking();
+      return;
     }
 
-    //? Store the first typed time of a frequent typed code inside typingStartTime within less than 10 seconds break
-    public trackTyping(event: vscode.TextDocumentChangeEvent): void{
-        const currentLanguage = vscode.window.activeTextEditor?.document.languageId || 'unknown';
+    this.typingTimer = setTimeout(() => {
+      const now = Date.now();
+      if (now - this.typingStartTime! >= 10000) {
+        // this.stopTracking();
+        console.log("User took more break than the interval");
+      }
+    }, 10000);
+  }
 
-        if (!this.isTyping) {
-            this.typingStartTime = Date.now();
-            this.isTyping = true;
-            this.typingStartLan = currentLanguage;
-            console.log("Typing started!");
-        }
+  private stopTracking(): void {
+    if (!this.isTyping || !this.typingStartTime) return;
 
-        if (this.typingTimer) clearTimeout(this.typingTimer);
-        if (currentLanguage !== this.typingStartLan) {
-            this.stopTracking();
-            return;
-        }
+    const duration = Date.now() - this.typingStartTime;
+    this.typingStartTime = null;
+    this.isTyping = false;
 
-        this.typingTimer = setTimeout(() => {
-            const now = Date.now();
-            if (now - this.typingStartTime! >= 10000) {
-                this.stopTracking();
-            }
-        }, 10000);
+    const manager = StatsManager.getInstance(this.context);
+    const apiKey = this.context.globalState.get<string>("rankDevsApiKey");
+    if (apiKey) {
+      manager.addTypingData(this.typingStartLan, duration, apiKey); //? {'js',120}
+    } else {
+      console.error("API key is missing!");
     }
+  }
 
-    private stopTracking(): void{
-        if (!this.isTyping || !this.typingStartTime) return;
-
-        const duration = Date.now() - this.typingStartTime;
-        this.typingStartTime = null;
-        this.isTyping = false;
-
-        const manager = StatsManager.getInstance(this.context);
-        const apiKey = this.context.globalState.get<string>('rankDevsApiKey');
-        if (apiKey) {
-            manager.addTypingData(this.typingStartLan, duration, apiKey); //? {'js',120}
-        } else {
-            console.error('API key is missing!');
-        }
+  public callApiService(): void {
+    const apiKey = this.context.globalState.get<string>("rankDevsApiKey");
+    if (apiKey) {
+      sendTypingDataToBackend(apiKey, this.context);
+    } else {
+      console.error("API key is missing!");
     }
-
-    public callApiService(): void{
-        const apiKey = this.context.globalState.get<string>('rankDevsApiKey');
-        if (apiKey) {
-            sendTypingDataToBackend(apiKey, this.context);
-        } else {
-            console.error('API key is missing!');
-        }
-    }
-
+  }
 }
