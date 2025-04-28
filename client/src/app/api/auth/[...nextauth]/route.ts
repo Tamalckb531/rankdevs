fe72a46b3098a0a8358431b3f04802981d8757ce
Tamal <ckbtamaldipnew@gmail.com>
2025-04-28 20:05:41 +0600

    Login feature -> Frontend -> add github OAuth route

---
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth";
+import type { AuthOptions, Account, Profile, Session, User } from "next-auth";
+
+import { JWT } from "next-auth/jwt";
+import GitHubProvider from "next-auth/providers/github";
+
+interface SignIn {
+  user: User;
+  account: Account | null;
+  profile?: Profile;
+}
+
+interface stu {
+  session: Session;
+  token: JWT;
+}
+
+const backendUrl = process.env.BACKEND_URL;
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ user, account, profile }: SignIn) {
+      console.log("Github Profile: ", profile);
+
+      await fetch(`${backendUrl}/api/auth/github`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          githubUsername: (profile as { login: string }).login,
+        }),
+      });
+
+      return true;
+    },
+
+    async session({ session, token }: stu) {
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
