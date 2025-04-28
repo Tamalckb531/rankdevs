d8252ce38e691f1c751bb82fac0e09744e7af941
Tamal <ckbtamaldipnew@gmail.com>
2025-04-28 20:53:41 +0600

    Login feature -> Frontend -> add sign in function and loading states in login form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -8,11 +9,32 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Github } from "lucide-react";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import Spinner from "./Spinner";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await signIn("github", { redirect: false });
+
+      if (response?.error) setError("Failed to login. Please try again");
+    } catch (error) {
+      setError("An unexpected error occurred. Please try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className=" bg-background">
@@ -25,8 +47,22 @@ export function LoginForm({
         <CardContent>
           <form>
             <div className="flex flex-col gap-6">
-              <Button variant="default" className="w-full">
-                <Github /> Login with Github
+              {error && <div className="text-red-500 text-center">{error}</div>}
+              <Button
+                variant="default"
+                className="w-full"
+                onClick={handleLogin}
+                disabled={loading}
+              >
+                {loading ? (
+                  <div className=" flex items-center justify-center gap-2">
+                    <Spinner /> <span>Please Wait....</span>
+                  </div>
+                ) : (
+                  <>
+                    <Github /> Login with Github
+                  </>
+                )}
               </Button>
             </div>
           </form>
