56ead4e8c1dc195517f6e4bf36faf515f4b134fd
Tamal <ckbtamaldipnew@gmail.com>
2025-04-29 22:54:25 +0600

    Frontend -> add type

---
+++ client/src/lib/type.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  githubUserName: string;
+  email: string;
+  apiKey: string;
+  twitterUsername: string;
+  peerlistLink: string;
+  leetcodeLink: string;
+  codeforcesLink: string;
+  linkedIn: string;
+}
