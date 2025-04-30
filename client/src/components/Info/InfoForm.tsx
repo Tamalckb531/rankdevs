6ffd8e0c30c7b402de3b0e830164dc4681650e72
Tamal <ckbtamaldipnew@gmail.com>
2025-04-30 19:52:57 +0600

    Info feature -> Design -> add new things

--- client/src/components/Info/InfoForm.tsx
+++ client/src/components/Info/InfoForm.tsx
@@ -3,8 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
-import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -17,33 +15,40 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Copy } from "lucide-react";
+import useUserStore from "@/store/useUserStore";
 
 const FormSchema = z.object({
-  apiKey: z.string().min(8).max(8),
+  apiKey: z.string().min(8),
   twitter: z.string().optional(),
   peerlist: z.string().optional(),
   leetcode: z.string().optional(),
   codeforce: z.string().optional(),
   portfolio: z.string().optional(),
+  email: z.string().email().optional(),
+  linkedIn: z.string().optional(),
 });
 
 export function InfoForm() {
+  const user = useUserStore((state) => state.user);
+  const setUser = useUserStore((state) => state.setUser);
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      apiKey: "abskieghtupw",
+      twitter: user?.twitterUsername || "",
+      peerlist: user?.peerlistLink || "",
+      leetcode: user?.leetcodeLink || "",
+      codeforce: user?.codeforcesLink || "",
+      portfolio: user?.portfolio || "",
+      email: user?.email || "",
+      linkedIn: user?.linkedIn || "",
+    },
+    values: {
+      apiKey: user?.apiKey || "",
     },
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast("Event has been created", {
-      description: "Sunday, December 03, 2023 at 9:00 AM",
-      action: {
-        label: "Undo",
-        onClick: () => console.log("Undo"),
-      },
-    });
-  }
+  function onSubmit(data: z.infer<typeof FormSchema>) {}
 
   return (
     <Form {...form}>
@@ -138,6 +143,34 @@ export function InfoForm() {
                 <Input placeholder="tamal.com" {...field} />
               </FormControl>
               <FormDescription>Add your Portfolio link here</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email : </FormLabel>
+              <FormControl>
+                <Input placeholder="ckbtamaldipnew@gmail.com" {...field} />
+              </FormControl>
+              <FormDescription>Add your Email account here</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="portfolio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>LinkedIn : </FormLabel>
+              <FormControl>
+                <Input placeholder="TamalCDip" {...field} />
+              </FormControl>
+              <FormDescription>Add your LinkedIn username here</FormDescription>
               <FormMessage />
             </FormItem>
           )}
