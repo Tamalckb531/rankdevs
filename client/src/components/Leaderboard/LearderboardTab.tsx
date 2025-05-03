9314495cbcee74b3820d4ae1affb091842c75900
Tamal <ckbtamaldipnew@gmail.com>
2025-05-03 20:12:56 +0600

    Leaderboard feature -> frontend -> add leaderboard tab to state integration

--- client/src/components/Leaderboard/LearderboardTab.tsx
+++ client/src/components/Leaderboard/LearderboardTab.tsx
@@ -1,25 +1,32 @@
+"use client";
 import React from "react";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import useTabStore from "@/store/userTabStore";
 
 const LeaderboardTab = () => {
+  const setTab = useTabStore((state) => state.setTab);
+
   return (
     <Tabs defaultValue="daily">
       <TabsList className=" bg-background">
         <TabsTrigger
           value="daily"
           className="text-lg md:text-xl md:p-5 p-3 py-4 rounded-lg"
+          onClick={() => setTab("daily")}
         >
           Daily
         </TabsTrigger>
         <TabsTrigger
           value="weekly"
           className="text-lg md:text-xl md:p-5 p-3 py-4  rounded-lg"
+          onClick={() => setTab("weekly")}
         >
           Weekly
         </TabsTrigger>
         <TabsTrigger
           value="monthly"
           className=" text-lg md:text-xl md:p-5 p-3 py-4 rounded-lg"
+          onClick={() => setTab("monthly")}
         >
           Monthly
         </TabsTrigger>
