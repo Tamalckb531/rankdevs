7113963d0037e7d03f8c2cc14d27503ee33fe9c9
Tamal <ckbtamaldipnew@gmail.com>
2025-05-04 10:11:06 +0600

    Info feature -> Frontend -> clear datatable data

--- client/src/components/Leaderboard/DataTable.tsx
+++ client/src/components/Leaderboard/DataTable.tsx
@@ -17,51 +17,6 @@ import { msToHM } from "@/lib/language";
 import { userInfo } from "@/lib/type";
 
 export function DataTable() {
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ];
-
   const { isPending, isError, error } = useLeaderboard();
   const ls = useLSSTore((state) => state.ls);
 
