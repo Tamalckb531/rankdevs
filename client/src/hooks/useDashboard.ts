69b246628822989358d074274bf68f3b0cdfa534
Tamal <ckbtamaldipnew@gmail.com>
2025-05-07 01:22:19 +0600

    Dashboard Feature -> Frontend -> add structure for batching

--- client/src/hooks/useDashboard.ts
+++ client/src/hooks/useDashboard.ts
@@ -2,11 +2,20 @@
 import { DashBoardPayload } from "@/lib/type";
 import useDashboardStore from "@/store/useDashboardStore";
 import { useMutation } from "@tanstack/react-query";
+import useDashboardBatch from "./useDashboardBatch";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const useDashboard = (userId: string) => {
   const setDashboard = useDashboardStore((state) => state.setDashboard);
+  const {
+    CalculateTotal,
+    CalculateWeekly,
+    CalculateMonthly,
+    CalculateYearly,
+    FetchLeetCode,
+    FetchGithub,
+  } = useDashboardBatch();
 
   const mutation = useMutation({
     mutationFn: async () => {
@@ -21,6 +30,25 @@ const useDashboard = (userId: string) => {
 
       const data: DashBoardPayload = await res.json();
       setDashboard(data);
+    },
+
+    onSuccess: async () => {
+      const queue = [
+        () => CalculateTotal(),
+        () => CalculateWeekly(),
+        () => CalculateYearly(),
+        () => CalculateMonthly(),
+        () => FetchLeetCode(),
+        () => FetchGithub(),
+      ];
+
+      for (const fn of queue) {
+        try {
+          await fn();
+        } catch (err) {
+          console.log("Batch failed: ", err);
+        }
+      }
     },
   });
 
