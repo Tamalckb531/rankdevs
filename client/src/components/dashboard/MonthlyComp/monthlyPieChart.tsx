8aedc2347f5d447aecee98433e752fa5370bb928
Tamal <ckbtamaldipnew@gmail.com>
2025-05-09 02:49:59 +0600

    Dashboard Feature -> Frontend -> add monthly line chart

--- client/src/components/dashboard/MonthlyComp/monthlyPieChart.tsx
+++ client/src/components/dashboard/MonthlyComp/monthlyPieChart.tsx
@@ -9,47 +9,15 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-const chartData = [
-  { day: "1", sum: 186 },
-  { day: "2", sum: 305 },
-  { day: "3", sum: 237 },
-  { day: "4", sum: 73 },
-  { day: "5", sum: 209 },
-  { day: "6", sum: 214 },
-  { day: "7", sum: 50 },
-  { day: "8", sum: 178 },
-  { day: "9", sum: 223 },
-  { day: "10", sum: 356 },
-  { day: "11", sum: 445 },
-  { day: "12", sum: 22 },
-  { day: "13", sum: 246 },
-  { day: "14", sum: 153 },
-  { day: "15", sum: 89 },
-  { day: "16", sum: 201 },
-  { day: "17", sum: 203 },
-  { day: "18", sum: 202 },
-  { day: "19", sum: 208 },
-  { day: "20", sum: 315 },
-  { day: "21", sum: 198 },
-  { day: "22", sum: 150 },
-  { day: "23", sum: 178 },
-  { day: "24", sum: 251 },
-  { day: "25", sum: 365 },
-  { day: "26", sum: 256 },
-  { day: "27", sum: 189 },
-  { day: "28", sum: 120 },
-  { day: "29", sum: 30 },
-  { day: "30", sum: 75 },
-  { day: "31", sum: 111 },
-];
-const chartConfig = {
-  sum: {
-    label: "Total",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
+import useMonthlyStatsStore from "@/store/useMonthlyStatsStore";
+import ChartToolTip from "@/components/ChartToolTip";
+import { getLanguageType, languages } from "@/lib/language";
+
+const chartConfig = {} satisfies ChartConfig;
 
 export function MonthlyPieChart() {
+  const monthlyStats = useMonthlyStatsStore((state) => state.monthlyStats);
+  const chartData = monthlyStats.data;
   return (
     <ChartContainer
       config={chartConfig}
@@ -61,22 +29,38 @@ export function MonthlyPieChart() {
         margin={{
           left: 12,
           right: 12,
+          top: 5,
         }}
       >
         <CartesianGrid vertical={false} />
         <XAxis
-          dataKey="day"
+          dataKey="field"
           tickLine={false}
           axisLine={false}
           tickMargin={10}
           tickFormatter={(value) => value.slice(0, 3)}
+          padding={{ left: 10, right: 10 }}
         />
         <ChartTooltip
-          cursor={true}
-          content={<ChartTooltipContent indicator="dot" />}
+          cursor={false}
+          content={
+            <ChartTooltipContent
+              hideLabel={false}
+              formatter={(value, name, entry, index, payload) => {
+                const ms = value as number;
+                return (
+                  <ChartToolTip
+                    fill={languages[entry.payload.field]?.color || "#f34b7d"}
+                    language={getLanguageType(entry.payload.field)}
+                    time={ms}
+                  />
+                );
+              }}
+            />
+          }
         />
         <Line
-          dataKey="sum"
+          dataKey="time"
           type="natural"
           stroke="#f34b7d"
           strokeWidth={2}
