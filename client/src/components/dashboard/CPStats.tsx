987edf8d9aa9f033552c4a26afa2862d69839323
Tamal <ckbtamaldipnew@gmail.com>
2025-05-11 22:57:04 +0600

    Dashboard Feature -> Frontend -> add different stats in cp stats

--- client/src/components/dashboard/CPStats.tsx
+++ client/src/components/dashboard/CPStats.tsx
@@ -2,8 +2,28 @@ import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import PlatfromSelect from "./CPComp/PlatfromSelect";
 import LeetcodeStats from "./CPComp/LeetcodeStats";
+import useLeetCodeStatsStore from "@/store/useLeetcodeStatsStore";
 
 const CPStats = () => {
+  const leetCodeStats = useLeetCodeStatsStore((state) => state.leetCodeStats);
+
+  if (!leetCodeStats || leetCodeStats.isError) {
+    <Card className="flex flex-col bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2">
+      <div className=" h-full w-full flex items-center justify-center text-xl text-red-400">
+        Can't show Leetcode Stats
+      </div>
+    </Card>;
+  }
+
+  if (!leetCodeStats.isLoading && !leetCodeStats.data) {
+    <Card className="flex flex-col bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2">
+      <Header />
+      <div className=" h-full w-full flex items-center justify-center text-xl text-blue-400">
+        Data is empty
+      </div>
+    </Card>;
+  }
+
   return (
     <Card className="flex flex-col bg-background shadow-md dark:shadow-slate-500 rounded-2xl px-2">
       <Header />
