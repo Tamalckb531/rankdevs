007f7b8b43dbaab349f30a9d91dacee018add005
Tamal <ckbtamaldipnew@gmail.com>
2025-05-11 23:45:42 +0600

    Dashboard Feature -> Frontend -> add leetcode stats without acceptance rate

--- client/src/components/dashboard/CPComp/LeetcodeStats.tsx
+++ client/src/components/dashboard/CPComp/LeetcodeStats.tsx
@@ -1,32 +1,44 @@
 import React from "react";
 import { ModePieChart } from "./ModePieChart";
 import LeetCodeStatsSkeleton from "@/components/Skeletons/LeetCodeSkeleton";
+import useLeetCodeStatsStore from "@/store/useLeetcodeStatsStore";
 
 const LeetcodeStats = ({ loading }: { loading: boolean }) => {
   if (loading) return <LeetCodeStatsSkeleton />;
+
+  const leetCodeStats = useLeetCodeStatsStore(
+    (state) => state.leetCodeStats.data
+  );
+
   return (
     <>
       <div className="info-rank flex items-center justify-between">
         <div className="info flex flex-col items-start h-full mt-10 leading-8">
-          <p className=" font-bold">ckbtamaldipnew</p>
+          <p className=" font-bold">{leetCodeStats?.username}</p>
           <p className=" text-sm text-slate-400">
             Contest Rating :{" "}
             <span className=" dark:text-green-400 font-bold text-green-600">
-              1,414
+              {Math.ceil(leetCodeStats?.contestRating || 0)}
             </span>
           </p>
           <p className=" text-sm text-slate-400">
             Global Rank :{" "}
             <span className=" dark:text-green-400 font-bold text-green-600">
-              5336,264
+              {leetCodeStats?.rank.toLocaleString()}
             </span>
           </p>
         </div>
         <div className="stat flex items-center justify-between gap-2 flex-wrap w-[50%]">
-          <StatCard topic="Contribution" stat="582" />
-          <StatCard topic="Contest" stat="1" />
-          <StatCard topic="Top" stat="78.71%" />
-          <StatCard topic="Acceptance" stat="80.33%" />
+          <StatCard
+            topic="Contribution"
+            stat={leetCodeStats?.contributionPoints || 0}
+          />
+          <StatCard
+            topic="Contest"
+            stat={leetCodeStats?.contestAttended || 0}
+          />
+          <StatCard topic="Top" stat={leetCodeStats?.topPercentage || 0} />
+          <StatCard topic="Acceptance" stat={0} />
         </div>
       </div>
       <p className="total text-sm font-bold gap-1 mt-1 flex items-center justify-center">
@@ -45,7 +57,7 @@ const LeetcodeStats = ({ loading }: { loading: boolean }) => {
 
 interface stat {
   topic: string;
-  stat: string;
+  stat: number;
 }
 
 const StatCard = ({ topic, stat }: stat) => {
@@ -54,7 +66,7 @@ const StatCard = ({ topic, stat }: stat) => {
       {topic === "Top" ? (
         <>
           <p className=" text-wrap text-xs">{topic}</p>
-          <p className=" font-bold">{stat}</p>
+          <p className=" font-bold">{stat}%</p>
         </>
       ) : (
         <>
