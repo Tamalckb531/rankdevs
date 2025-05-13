dcfed24155c8f30288670ebc3fe7ad0c8af274fb
Tamal <ckbtamaldipnew@gmail.com>
2025-05-14 01:36:21 +0600

    Dashboard Feature -> Frontend -> add CF component

--- client/src/components/dashboard/CPComp/CodeForceStats.tsx
+++ client/src/components/dashboard/CPComp/CodeForceStats.tsx
@@ -40,18 +40,43 @@ const CodeForceStats = () => {
             </span>
           </p>
         </div>
-        <div className="stat flex items-center justify-between gap-2 flex-wrap w-[40%] text-slate-400 text-lg">
-          <div className=" flex items-center justify-between w-full">
+        <div className="stat flex flex-col items-center justify-between gap-2 w-[40%] text-slate-400 text-lg">
+          <div
+            className=" flex items-center justify-between w-full"
+            title="legendary grandmaster"
+          >
             On Prime :{" "}
             {<CFRankCard rank={primeRank.name} color={primeRank.color} />}
           </div>
-          <div className="flex items-center justify-between w-full">
+          <div
+            className="flex items-center justify-between w-full"
+            title="specialist"
+          >
             Now : {<CFRankCard rank={newRank.name} color={newRank.color} />}
           </div>
         </div>
+      </div>
+      <div className="stat flex items-center justify-between mt-2">
+        <StatCard topic="Contest Attended" stat={15} />
+        <StatCard topic="Friends" stat={3} />
+        <StatCard topic="Contributions" stat={9} />
       </div>
     </>
   );
 };
 
 export default CodeForceStats;
+
+interface stat {
+  topic: string;
+  stat: number;
+}
+
+const StatCard = ({ topic, stat }: stat) => {
+  return (
+    <div className="flex flex-col gap-1 justify-center items-center mt-2">
+      <p className=" font-bold text-lg">{stat}</p>
+      <p className=" text-wrap text-xs text-slate-400">{topic}</p>
+    </div>
+  );
+};
