3498333b4b7245a6081aa362588e6dcfae14b9c2
Tamal <ckbtamaldipnew@gmail.com>
2025-05-17 23:28:25 +0600

    Design -> cancel out the Start now component

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import HeroSection from "./Landing/HeroSection";
 import Features from "./Landing/Features";
-import StartNow from "./Landing/StartNow";
 
 const Landing = () => {
   return (
     <div className=" flex flex-col gap-2 ">
       <HeroSection />
       <Features />
-      <StartNow />
     </div>
   );
 };
--- client/src/components/Landing/StartNow.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StartNow = () => {
-  return <div>StartNow</div>;
-};
-
-export default StartNow;
