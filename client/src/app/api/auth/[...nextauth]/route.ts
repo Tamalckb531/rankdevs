a26b300ed6750686b5d752b4f64752eb62a03d24
Tamal <ckbtamaldipnew@gmail.com>
2025-05-22 21:21:41 +0600

    change in type

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import { authOptions } from "@/app/utils/authOptions";
 import NextAuth from "next-auth/next";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as any;
 export { handler as GET, handler as POST };
