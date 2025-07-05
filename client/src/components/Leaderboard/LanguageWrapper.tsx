486df0dc9bfb381daca96c67e6fdf98c201f383e
Tamal <ckbtamaldipnew@gmail.com>
2025-07-06 00:36:21 +0600

    Frontend -> Leaderboard -> set up 5 on table and see all button option

--- client/src/components/Leaderboard/LanguageWrapper.tsx
+++ client/src/components/Leaderboard/LanguageWrapper.tsx
@@ -2,15 +2,23 @@ import { Stats } from "@/lib/type";
 import React from "react";
 import { msToHM } from "@/lib/language";
 import { LanguageCard } from "./LanguageCard";
+import { Ellipsis } from "lucide-react";
 
 export const LanguageWrapper = ({ stats }: { stats: Stats }) => {
   return (
     <div className=" flex items-center justify-start gap-2">
       {Object.entries(stats)
         .filter(([key]) => key !== "total")
+        .slice(0, 5)
         .map(([language, ms]) => (
           <LanguageCard key={language} language={language} time={msToHM(ms)} />
         ))}
+      <div
+        title="See all"
+        className=" cursor-pointer hover:border border-red-300 rounded-full"
+      >
+        <Ellipsis />
+      </div>
     </div>
   );
 };
